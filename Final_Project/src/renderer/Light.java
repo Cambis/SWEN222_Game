69719ec940b090920cd369e3541cbe92ebd6631e
thompsstep2 <thompsstep2@pie-cart.ecs.vuw.ac.nz>
2015-10-06 10:02:33 +1300

    Pushed missing light class

---
+++ Final_Project/src/renderer/Light.java
@@ -0,0 +1,28 @@
+package renderer;
+
+import renderer.math.Vec3;
+
+public class Light {
+	private Vec3 position;
+	private Vec3 direction;
+
+	public Light(Vec3 position, Vec3 direction) {
+		super();
+		this.position = position.add(new Vec3(0, 0.2f, 0));
+		this.direction = new Vec3(Math.sin(direction.getY()), -0.1f, Math.cos(direction.getY()));
+	}
+	public Vec3 getPosition() {
+		return position;
+	}
+	public void setPosition(Vec3 position) {
+		this.position = position;
+	}
+	public Vec3 getDirection() {
+		return direction;
+	}
+	public void setDirection(Vec3 direction) {
+		this.direction = direction;
+	}
+
+
+}
