f2acdc2b8fcf01adfebdebff831e5a89a6d8c617
Cameron Bryers <cambo_the_rambo@hotmail.com>
2015-10-08 11:20:04 +1300

    Merge Issue Incoming

--- Final_Project/src/game/logic/Player.java
+++ Final_Project/src/game/logic/Player.java
@@ -18,7 +18,7 @@ import renderer.math.Vec3;
  * Represents a player in the game. This class should do the player logic and
  * not have anything to do with the client/ server.
  *
- * @author Bieleski, Bryers, Gill & Thompson MMXV.
+ * @author Bieleski, Bryers, Gill and Thompson MMXV.
  *
  */
 public class Player {
