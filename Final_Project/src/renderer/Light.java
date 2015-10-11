047f91ac6760a677123adef053c842019ac3d6b1
thompsstep2 <thompsstep2@sen-sen.ecs.vuw.ac.nz>
2015-10-11 15:28:43 +1300

    Commenting Added commenting to the classes in the renderer package and deleted unused classes

--- Final_Project/src/renderer/Light.java
+++ Final_Project/src/renderer/Light.java
@@ -2,11 +2,32 @@ package renderer;
 
 import renderer.math.Vec3;
 
+/**
+ * This class represents a player's spotlight
+ *
+ * @author Stephen Thompson
+ *
+ */
 public class Light {
+	// the light's position
 	private Vec3 position;
+
+	// the light's direction
 	private Vec3 direction;
+
+	// the team this light belongs to
 	private R_Player.Team side;
 
+	// the cutoff angle of the spotlight
+	private float cutoff;
+
+	/**
+	 * The constructor for a light object
+	 *
+	 * @param position	the light's position
+	 * @param direction the light's direction
+	 * @param side		the light's team
+	 */
 	public Light(Vec3 position, Vec3 direction, R_Player.Team side) {
 		super();
 		float xx = (float)Math.sin(direction.getY());
@@ -14,23 +35,63 @@ public class Light {
 		this.position = position.add(new Vec3(xx*0.1f, 0.2f, zz*0.1f));
 		this.direction = new Vec3(xx, -0.5f, zz);
 		this.side = side;
+		this.cutoff = 0.8f;
 	}
+
+	/**
+	 * Gets the light's position
+	 * @return 	the light's position
+	 */
 	public Vec3 getPosition() {
 		return position;
 	}
+
+	/**
+	 * Sets the light's position
+	 * @param position - the new position
+	 */
 	public void setPosition(Vec3 position) {
 		this.position = position;
 	}
+
+	/**
+	 * Gets the light's direction
+	 * @return	the light's direction
+	 */
 	public Vec3 getDirection() {
 		return direction;
 	}
+
+	/**
+	 * Sets the light's direction
+	 * @param direction	- the new direction
+	 */
 	public void setDirection(Vec3 direction) {
 		this.direction = direction;
 	}
+
+	/**
+	 * Gets the light's team
+	 * @return 	the light's team
+	 */
 	public R_Player.Team getSide() {
 		return side;
 	}
+
+	/**
+	 * Sets the team of the player
+	 *
+	 * @param side	the new team side
+	 */
 	public void setSide(R_Player.Team side) {
 		this.side = side;
+	}
+
+	/**
+	 * Gets the cutoff angle of the spotlight
+	 * @return the cutoff angle
+	 */
+	public float getCutoff() {
+		return cutoff;
 	}
 }
--- Final_Project/src/renderer/R_AbstractModel.java
+++ Final_Project/src/renderer/R_AbstractModel.java
@@ -1,19 +1,41 @@
 package renderer;
 
-import java.awt.image.BufferedImage;
 import java.util.List;
 
 import renderer.math.Mat4;
 import renderer.math.Vec3;
 
+/**
+ *	This class holds a R_AbstractModelData object and draws it in the correct position, orientation and scale
+ *
+ * @author Stephen Thompson
+ *
+ */
 public abstract class R_AbstractModel {
+	// the model's name
 	private final String name;
+
+	// the model's data object
 	private final R_AbstractModelData model;
 
+	// the model's position
 	private Vec3 position;
+
+	// the model's orientation
 	private Vec3 orientation;
+
+	// the model's scale
 	private Vec3 scale;
 
+
+	/**
+	 * @param name -the model's name
+	 * @param model - the model data to use, this model does not hold the actual data, but rather draws the modeldata at the correct position
+	 * 				  *Note that only one modeldata object is required for the enitre program
+	 * @param position - the model's position
+	 * @param orientation - the model's orientation
+	 * @param scale - the model's scale
+	 */
 	public R_AbstractModel(String name, R_AbstractModelData model, Vec3 position, Vec3 orientation, Vec3 scale) {
 		super();
 		this.position = position;
@@ -23,38 +45,77 @@ public abstract class R_AbstractModel {
 		this.model = model;
 	}
 
+	/**
+	 * @return - returns the model's position
+	 */
 	public Vec3 getPosition() {
 		return position;
 	}
 
+	/**
+	 * @param position - sets the model's position
+	 */
 	public void setPosition(Vec3 position) {
 		this.position = position;
 	}
 
+	/**
+	 * @return - returns the model's orientation
+	 */
 	public Vec3 getOrientation() {
 		return orientation;
 	}
 
+	/**
+	 * @param orientation - sets the model's orientation
+	 */
 	public void setOrientation(Vec3 orientation) {
 		this.orientation = orientation;
 	}
 
+	/**
+	 * @return - returns the model's scale
+	 */
 	public Vec3 getScale() {
 		return scale;
 	}
 
+	/**
+	 * @param scale - sets the model's scale
+	 */
 	public void setScale(Vec3 scale) {
 		this.scale = scale;
 	}
 
+	/**
+	 * @return - returns the model's name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * @return - returns the model's modeldata object
+	 */
 	protected R_AbstractModelData getModel(){
 		return model;
 	}
 
+
+	/**
+	 * Draws the model to 2 buffers - the viewport and the zBuffer
+	 *tileSize
+	 * @param viewport	the buffer to draw the pixels to
+	 * @param zBuffer	the zbuffer used to determine whether to draw the pixel
+	 * @param width		the width of the screen
+	 * @param height	the height of the screen
+	 * @param viewProjMatrix	the matrix used to convert the vertices into screen space
+	 * @param lights	a list of lights to use during the render
+	 * @param side		the team's side to view
+	 * @param visible	the object's team
+	 * @param shadowMap	the 2d array of the map's layout
+	 * @param tileSize	the tile's size used to map the positions to the shadowmap
+	 */
 	protected void draw(int[] viewport, float[][] zBuffer, int width, int height, Mat4 viewProjMatrix, List<Light> lights,
 										R_Player.Team side, R_Player.Team visible, int[][] shadowMap, int tileSize) {
 		// Translate to position
